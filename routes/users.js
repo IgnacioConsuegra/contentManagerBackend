753b170c3805bc42a87a0b51e9e857705eea5da9
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2026-05-24 20:56:00 -0500

    fourth commit

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
-import { isDeveloper } from "../middleware/devMiddleWare.js";
+import { isDeveloper } from "../middleware/devMiddleware.js";
 
 const router = express.Router();
 
